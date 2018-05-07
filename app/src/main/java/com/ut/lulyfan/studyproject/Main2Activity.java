bc91d3de48f5fe62b51cc13cdf2e6d02b4bd2fd4
lulyfan <18807644294@163.com>
2018-05-08 00:24:12 +0800

    toolbar

--- app/src/main/java/com/ut/lulyfan/studyproject/Main2Activity.java
+++ app/src/main/java/com/ut/lulyfan/studyproject/Main2Activity.java
@@ -1,18 +1,16 @@
 package com.ut.lulyfan.studyproject;
 
-import android.content.Intent;
-import android.os.Bundle;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
-import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
+public class Main2Activity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_main2);
 
         Toolbar myToolbar = (Toolbar) findViewById(R.id.my_toolbar);
         setSupportActionBar(myToolbar);
@@ -20,10 +18,5 @@ public class MainActivity extends AppCompatActivity {
         ActionBar ab = getSupportActionBar();
         // Enable the Up button
         ab.setDisplayHomeAsUpEnabled(true);
-    }
-
-    public void onClick(View view) {
-        Intent intent = new Intent(this, Main2Activity.class);
-        startActivity(intent);
     }
 }
